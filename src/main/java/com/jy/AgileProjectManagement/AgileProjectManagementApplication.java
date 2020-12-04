3597cbd6eaffa6deef91091b1ebdd72cf9372e54
Quan Nguyen <nguyen.hong.quan282@gmail.com>
2020-12-04 09:20:17 +0700

    init

---
+++ src/main/java/com/jy/AgileProjectManagement/AgileProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jy.AgileProjectManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileProjectManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jy/AgileProjectManagement/AgileProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.AgileProjectManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
